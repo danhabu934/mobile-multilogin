1225c7a868be03bcfe3149bb9e60e51f1cef6a70
danhabu934 <danhabu934@gmail.com>
2026-09-17 04:20:18 -0300

    feat: mount persistent session controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ProfileRestoreLauncher from './ProfileRestoreLauncher'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
+  <React.StrictMode><App/><ProfileRestoreLauncher/></React.StrictMode>,
 )
